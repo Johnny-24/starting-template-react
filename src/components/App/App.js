624e4ad899242eb19302e9e5be96a1801d810a35
Johnny <dos_24@inbox.ru>
2022-01-10 12:27:41 +0500

    init react

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { HomePage } from './../../pages/HomPage';
+
+const App = () =>  {
+  return (
+    <header>
+      App
+    </header>
+  );
+}
+
+export default App;
